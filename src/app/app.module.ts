008115477c44457c0a497303dd42c62ac6a9a5b0
Marcin <marcin9195@vp.pl>
2020-05-13 15:08:34 +0200

    3867

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlgorytmyComponent } from './algorytmy/algorytmy.component';
+import {AppRoutingModule} from './app-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlgorytmyComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
