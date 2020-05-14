615ab8d0e03d77572cee234995198004f640b180
Marcin <marcin9195@vp.pl>
2020-05-14 13:29:14 +0200

    cd...

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,6 +9,9 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
+
+
+
   ngOnInit(): void {
   }
 
--- src/app/algorytmy/algorytmy.component.ts
+++ src/app/algorytmy/algorytmy.component.ts
@@ -7,28 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlgorytmyComponent implements OnInit {
   array1 = [];
-  originalNumbers1D: number[] = [1, 2, 6, 66, 66 , 66, 55, 44, 33, 12, 21, 22];
+  primaryArray: number[] = [1, 2, 6, 66, 66 , 66, 55, 55, 66, 44, 33, 12, 21, 22, 3000];
 
   constructor() { }
 
   ngOnInit(): void {
   }
   alfabetycznie(){
-    this.array1= this.originalNumbers1D.sort();
+    this.array1 = this.primaryArray.sort();
   }
   rosnaco(){
-   this.array1= this.originalNumbers1D.sort((a,b) => a - b);
+   this.array1 = this.primaryArray.sort((a, b) => a - b);
+
+
   }
+ dublowanie(){
+this.array1= this.primaryArray.filter((item, index) => this.primaryArray.indexOf(item) === index);
+ }
+pieciocyfrowe(){
+    this.array1= this.primaryArray.filter(v=>v%3===0 && v>=1000 && v<10000);
+}
   evenElements(){
-    this.array1 = this.originalNumbers1D.filter(v=>v%2===0);
+    this.array1 = this.primaryArray.filter(v=> v % 2 === 0);
   }
   divisibleBy4(){
-    this.array1 = this.originalNumbers1D.filter(v=>v%4===0);
+    this.array1 = this.primaryArray.filter(v=>v%4===0);
   }
   notDivisibleBy4() {
-    this.array1 = this.originalNumbers1D.filter(v=>v%4!==0);
+    this.array1 = this.primaryArray.filter(v=>v%4!==0);
   }
-  notDivisibleBy5(){
-    this.array1 = this.originalNumbers1D.filter(v=>v%5!==0);
-  }
+
 }
